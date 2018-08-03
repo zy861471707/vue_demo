56cada9e09c4de3a5a8df7e67084d6d57c86abe4
zhangyang <861471707@qq.com>
2018-08-04 00:17:29 +0800

    完成el-select的开发

--- src/components/el-select.vue
+++ src/components/el-select.vue
@@ -27,7 +27,8 @@ export default {
     },
     data(){
         return {
-            selected:''
+            selected:'',
+            wholeParent:''
         }
     },
     methods:{
@@ -36,9 +37,16 @@ export default {
         }
     },
     created(){
-        //这里进行初始化,这里还需要进行push一条‘全部’的数据,采用循环data里面的属性进行赋值，然后再push进去
         this.selected = this.isSelectInit;
-        console.log(this.selected)
+        let dataObj = {
+            label:'全部',
+            value:''
+        };
+        for(let [index,elem] of this.SelectData.entries()){
+            dataObj.value += elem.value +',';
+        }
+        dataObj.value = dataObj.value.slice(0,-1);
+        this.SelectData.unshift(dataObj);//添加到数组首位
     }
 
 }
